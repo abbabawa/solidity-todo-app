af28212b0c5d3e4d5a2fdad8cac444a04f4f3445
abba-bawa <abbabawa@punch.cool>
2022-10-26 03:49:17 +0100

    update todo function and test worked on

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -6,16 +6,26 @@ contract Todo {
     struct todoItem{
         uint id;
         string todo;
+        bool done;
     }
     mapping(address=>todoItem[]) todos;
 
     function addTodo(string memory todo) public  {
         uint count = todos[msg.sender].length; 
-        todos[msg.sender].push(todoItem({id: count, todo: todo}));
+        todos[msg.sender].push(todoItem({id: count, todo: todo, done: false}));
         
     }
 
     function getTodos() public view returns(todoItem[] memory){
         return todos[msg.sender];
+    }
+
+    function updateTodos(uint id, string memory todo, bool done) public {
+        require(id < todos[msg.sender].length && id >= 0, 'The id provided is out of range');
+        
+        todoItem storage item = todos[msg.sender][id];
+        // todos[msg.sender][id] = todoItem({id: id, todo: todo, done: done});
+        item.done = done;
+        item.todo = todo;
     }
 }
--- test/Todo.test.js
+++ test/Todo.test.js
@@ -1,24 +1,31 @@
 const Todo = artifacts.require("Todo");
-contract('Todo', (accounts) => {
-    let todo
-    before(async () => {
-        todo = await Todo.deployed()
-    
-      })
-    
-      it('It should add a todo item', async () => {
-        const addedTodo = await todo.addTodo("Take my bathe") 
-        let todos = await todo.getTodos.call();console.log(todos, typeof todos);
-        assert.equal(todos.length, 1)
-      })
+contract("Todo", (accounts) => {
+  let todo;
+  before(async () => {console.log("before each")
+    todo = await Todo.deployed();
+  });
 
-      it('It should get a users todos',async()=>{
-        let todos = await todo.getTodos.call();
-        assert.equal(todos.length, 1)
+  it("It should add a todo item", async () => {
+    const addedTodo = await todo.addTodo("Take my bathe");
+    let todos = await todo.getTodos.call();
+    // console.log(todos, typeof todos);
+    assert.equal(todos.length, 1);
+  });
 
-        const addedTodo = await todo.addTodo("brush my teeth") 
-        todos = await todo.getTodos.call();console.log(todos, typeof todos);
-        assert.equal(todos.length, 2)
-      })
-      
+  it("It should get a users todos", async () => {
+    let todos = await todo.getTodos.call();
+    assert.equal(todos.length, 1);
+
+    const addedTodo = await todo.addTodo("brush my teeth");
+    todos = await todo.getTodos.call();
+    console.log(todos[1].todo, typeof todos);
+    assert.equal(todos.length, 2);
+  });
+
+  it("It should get update todo item given its id", async () => {
+    let todos = await todo.getTodos.call();
+    let addedTodo = await todo.updateTodos(todos.length - 1, "Dress up", true)
+    todos = await todo.getTodos.call();
+    assert.equal(todos[todos.length - 1].todo, 'Dress up');
+  });
 });
