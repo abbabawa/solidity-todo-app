2d0df6747819833acc1b75b483040c3a3fee9707
abbabawa <abbabawa6@gmail.com>
2023-01-31 18:57:54 +0100

    category CRUD functions added

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -3,16 +3,50 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Todo {
+    struct categoriesStruct{
+        uint id;
+        string name;
+    }
     struct todoItem{
         uint id;
+        uint category;
         string todo;
         bool done;
     }
     mapping(address=>todoItem[]) todos;
+    mapping(address=>categoriesStruct[]) categories; 
 
-    function addTodo(string memory todo) public  {
+    function addCategory(string memory name) public {
+        uint count = categories[msg.sender].length; 
+        categories[msg.sender].push(categoriesStruct({id: count, name: name}));
+    }
+
+    function getCategories() public view returns(categoriesStruct[] memory){
+        return categories[msg.sender];
+    }
+
+    function updateCategory(uint id, string memory name) public {
+        uint count = categories[msg.sender].length; 
+        categories[msg.sender].push(categoriesStruct({id: count, name: name}));
+
+        require(id < categories[msg.sender].length && id >= 0, 'The id provided is out of range');
+        
+        categoriesStruct storage category = categories[msg.sender][id];
+        category.name = name; 
+    }
+
+    function deleteCategory(uint id) public {
+        require(id < categories[msg.sender].length && id >= 0, 'The id provided is out of range');
+
+        for(uint i = id; i < categories[msg.sender].length - 1; i++) {
+            categories[msg.sender][i] = categories[msg.sender][i+1];
+        }
+        categories[msg.sender].pop();
+    }
+
+    function addTodo(string memory todo, uint category) public  {
         uint count = todos[msg.sender].length; 
-        todos[msg.sender].push(todoItem({id: count, todo: todo, done: false}));
+        todos[msg.sender].push(todoItem({id: count, todo: todo, done: false, category: category}));
         
     }
 
